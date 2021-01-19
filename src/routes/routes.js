bd440c9e1c043172aa43ead5ec8c6ac831427518
Santiago Aguirre <aguirresantiago81@gmail.com>
2021-01-19 14:22:41 -0300

    Agregadas las rutas para que coincidan con todos los metodos de los controllers

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,12 +5,14 @@ import * as NavesController from './../controllers/NavesController';
 
 const router = Router();
 
+//home route
 router.get('/', (req, res)=>{
     res.json({
         message: 'Bienvenido al index de mi aplicación'
     })
 });
 
+//rutas de los personajes
 router.get('/personajes', PersonajesController.todosLosPersonajes);
 
 router.get('/personajes-alfb', PersonajesController.personajesPorNombre);
@@ -20,8 +22,19 @@ router.get('/personaje/:id', PersonajesController.personajePorId);
 router.get('/personaje-por-nombre/:nombre', PersonajesController.personajePorNombre);
 
 
+//rutas de los planetas
 router.get('/planetas', PlanetasController.todosLosPlanetas);
 
+router.get('/planetas-alfb', PlanetasController.planetasPorNombre);
+
+router.get('/planeta/:id', PlanetasController.planetaPorId);
+
+
+//rutas de las naves
 router.get('/naves', NavesController.todasLasNaves);
+
+router.get('/naves-alfb', NavesController.todasLasNaves);
+
+router.get('/naves/:id', NavesController.todasLasNaves);
 
 export default router;
